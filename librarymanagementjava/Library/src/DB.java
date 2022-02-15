26ed15065c094e65791d177b6e26927c28a4dd93
MayorFresh <danielmayotar@gmail.com>
2022-02-15 23:52:18 +0100

    updated database

--- librarymanagementjava/Library/src/DB.java
+++ librarymanagementjava/Library/src/DB.java
@@ -5,7 +5,7 @@ public class DB {
 	public static Connection getConnection(){
 		Connection con=null;
 		try{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","mayorfresh");
 		}catch(Exception e){System.out.println(e);}
 		return con;
